60bb884a003dccdad81bcd8634afa0367eb16dfd
David Chaves <davi.ch.fe@gmail.com>
2020-10-11 00:06:29 -0300

    Ajustando responsividade e começando a tela de banco-agendamentos

--- Tela-de-historico-de-doacoes/js-doador/dashboard-historico-doador.js
+++ Tela-de-historico-de-doacoes/js-doador/dashboard-historico-doador.js
@@ -9,26 +9,28 @@ let larguraDaJanela = window.innerWidth;
 console.log(larguraDaJanela);
 
 // regra para abrir o menu quando clicar no menu no canto esquerdo da tela
-if(larguraDaJanela <= 768) {
-    btnAbrir.addEventListener("click", function openNav() {
-        mySidenav.style.width = "180px";
-        principal.style.padding = "0 0 0 180px";
-    });
-}
-    if(larguraDaJanela <= 450) {
+
+    if(larguraDaJanela <= 360) {
         btnAbrir.addEventListener("click", function openNav() {
             mySidenav.style.width = "150px";
-            principal.style.padding = "0 0 0 150px";
+            principal.style.padding = "0 0 0 0px";
         });
     }
 
-        if(larguraDaJanela >= 1200) {
-            btnAbrir.addEventListener("click", function openNav() {
-                mySidenav.style.width = "200px";
-                principal.style.padding = "0 0 0 200px";
-                    
-            });
-        }
+    if(larguraDaJanela <= 768) {
+        btnAbrir.addEventListener("click", function openNav() {
+            mySidenav.style.width = "180px";
+            principal.style.padding = "0 0 0 0px";
+        });
+    }
+
+    if(larguraDaJanela > 768) {
+        btnAbrir.addEventListener("click", function openNav() {
+            mySidenav.style.width = "200px";
+            principal.style.padding = "0 0 0 200px";
+                
+        });
+    }
 
 // regra para fechar o menu lateral
 btnFechar.addEventListener("click", function closeNav() {
